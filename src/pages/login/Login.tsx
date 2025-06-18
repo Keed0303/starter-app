3bacefa02e3c4500080da01a754cf9642dafad25
Manuel Kyd Thomas Nagpala <103329030+Keed0303@users.noreply.github.com>
2025-06-18 11:21:41 +0800

    chore: add error handling message

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,28 @@
-import { IonButton, IonContent, IonHeader, IonImg, IonInput, IonInputPasswordToggle, IonLabel, IonPage, IonText, IonTitle } from "@ionic/react"
+import { IonButton, IonContent, IonImg, IonInput, IonInputPasswordToggle, IonLabel, IonPage } from "@ionic/react"
 import './Login.css';
-import BlobComponent from "../../components/Blob";
-import { Style } from "@capacitor/status-bar";
+import { useState } from "react";
+import ErrorMessage from "../../components/ErrorMessage";
+import { useHistory } from "react-router-dom";
 const Login: React.FC = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorUsernameMessage, setErrorUsernameMessage] = useState('');
+  const [errorPasswordMessage, setErrorPasswordMessage] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = () => {
+    if (username.trim() === '') {
+      setErrorUsernameMessage('Username cannot be blank.');
+    }
+
+    if (username.trim() === '') {
+      setErrorPasswordMessage('Password cannot be blank.');
+    }
+  }
+
+
+
   return (
     <IonPage>
       <IonContent fullscreen>
@@ -18,63 +38,52 @@ const Login: React.FC = () => {
             overflow: 'hidden',
           }}
         >
-          <svg viewBox="0 0 540 960"
-            preserveAspectRatio="xMidYMid slice"
+            <svg
             xmlns="http://www.w3.org/2000/svg"
-            style={{
-              width: '100%',
-              height: '100%',
-              // Optional for some browsers
-              }}
-              >
-                <rect x="0" y="0" width="540" height="960" fill="#ffffff"/>
-                <defs>
-                  <linearGradient id="grad1_0" x1="0%" y1="100%" x2="100%" y2="0%">
-                    <stop offset="30%" stopColor="#533cfa" stopOpacity="1"/>
-                    <stop offset="70%" stopColor="#533cfa" stopOpacity="1"/>
-                  </linearGradient>
-                </defs>
-                <defs>
-                  <linearGradient id="grad2_0" x1="0%" y1="100%" x2="100%" y2="0%">
-                    <stop offset="30%" stopColor="#533cfa" stopOpacity="1"/>
-                    <stop offset="70%" stopColor="#533cfa" stopOpacity="1"/>
-                  </linearGradient>
-                </defs>
-                <g transform="translate(540, 960)">
-                <path
-                  fill="url(#grad1_0)" d="M-432 0C-403.2 -30.4 -374.4 -60.8 -366.1 -98.1C-357.7 -135.4 -369.8 -179.7 -349.9 -202C-329.9 -224.3 -278 -224.6 -254.6 -254.6C-231.1 -284.5 -236.3 -344 -216 -374.1C-195.7 -404.2 -150 -404.8 -109.7 -409.6C-69.5 -414.3 -34.7 -423.1 0 -432L0 0Z" ></path>
-                </g>
-                <g transform="translate(0, 0)">
-                  <path fill="url(#grad2_0)" d="M432 0C388.5 27.4 345 54.7 338.1 90.6C331.2 126.5 360.8 170.9 362 209C363.2 247.1 335.9 279 305.5 305.5C275 332 241.4 353.2 204.5 354.2C167.6 355.3 127.3 336.2 92.7 345.8C58 355.4 29 393.7 0 432L0 0Z"/>
-                </g>
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            id="visual"
+            viewBox="0 0 540 960"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="none"
+            version="1.1">
+            <rect
+              x="0"
+              y="0"
+              width="540"
+              height="960"
+              fill="#0066FF"
+            />
+            <g transform="translate(240.75762979359584 433.128117504809)">
+              <path
+                d="M325 -294.5C421.3 -228.6 499.6 -114.3 514.1 14.4C528.5 143.2 479 286.4 382.7 368.2C286.4 450 143.2 470.5 32 438.5C-79.2 406.5 -158.4 322.1 -257.9 240.2C-357.4 158.4 -477.2 79.2 -455.5 21.7C-433.8 -35.8 -270.6 -71.6 -171.1 -137.4C-71.6 -203.3 -35.8 -299.1 39.3 -338.4C114.3 -377.6 228.6 -360.3 325 -294.5" fill="#ffffff"
+              />
+            </g>
           </svg>
         </div>
-
-  
-
        
-        <IonImg className="logo" src="/favicon.png"></IonImg>
         <form action="" className="form">
-          <IonLabel className="heading">
-            <h3>Greetings!</h3>
-            <p>Sign your account</p>
-          </IonLabel>
-          <IonInput className="form-input" label="Username" labelPlacement="floating" fill="solid" ></IonInput>
+          <div className="heading-container">
+            <IonImg className="logo" src="/favicon.png"></IonImg>
+            <IonLabel className="heading">
+              <h3>Greetings!</h3>
+              <p>Sign your account</p>
+            </IonLabel>
+          </div>
+          <IonInput className="form-input" label="Username" labelPlacement="floating" fill="solid"></IonInput>
+          <ErrorMessage error={errorUsernameMessage} />
+
           <IonInput className="form-input" label="Password" type="password" labelPlacement="floating" fill="solid" placeholder="Enter text">
             <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
           </IonInput>
-
+          <ErrorMessage error={errorPasswordMessage} />
           <IonLabel class="forgot-text">
             <p>Forgot your password?</p>
           </IonLabel>
-
-          <IonButton className="button-login" shape="round">Login</IonButton>
+          <IonButton className="button-login" shape="round" onClick={handleSubmit}>Login</IonButton>
         </form>
 
-        <IonLabel class="register"><p>Dont have an account? <span>Register</span></p></IonLabel>
-
-        {/* </BlobComponent> */}
-
+        <IonLabel class="register"><p>Dont have an account? <span className="link" onClick={() => history.push('/register')} >Register</span></p></IonLabel>
       </IonContent>
     </IonPage>
   )
