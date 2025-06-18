248c9e880fb8d6b49196a22e174fb5a249a09afb
Manuel Kyd Thomas Nagpala <103329030+Keed0303@users.noreply.github.com>
2025-06-18 11:21:56 +0800

    chore: add register page

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <>
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: -1,
+            overflow: 'hidden',
+          }}
+        >
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" id="visual" viewBox="0 0 540 960" width="540" height="960" version="1.1"><rect x="0" y="0" width="540" height="960" fill="#0066FF"/><g transform="translate(368.95066975513635 512.3794079741592)"><path d="M223.4 -256.1C280.4 -166.4 311.2 -83.2 305.4 -5.8C299.6 71.6 257.2 143.2 200.2 205.5C143.2 267.9 71.6 320.9 -35.8 356.7C-143.2 392.5 -286.4 411 -380.2 348.7C-474 286.4 -518.5 143.2 -499.8 18.7C-481.2 -105.8 -399.3 -211.7 -305.5 -301.3C-211.7 -391 -105.8 -464.5 -11.3 -453.2C83.2 -441.9 166.4 -345.7 223.4 -256.1" fill="#ffffff"/></g></svg>
+      </div>
+    </>
+  )
+}
+
+export default Register
